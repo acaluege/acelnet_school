43f30b929239f712c06a04f21700d0392d199c2e
Afonso Caluege <acaluege@outlook.com>
2024-10-31 17:32:16 +0100

    08 add css files

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="utf-8">
     <title>Home</title>    
+    
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/assets/all.min.css">   
 </head>
 <body>
-    <h1>Este é o Formulario do Home</h1>
+    
+    <div class="container-fluid">
+        <h1>Este é o Formulario do Home</h1>
+    </div>
+    
 </body>
 </html>
